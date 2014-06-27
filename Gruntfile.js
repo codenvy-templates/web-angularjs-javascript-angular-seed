b0dd744fd9879febbaf9341e363a79d3ffa471a0
Florent Benoit <Florent.Benoit@serli.com>
2014-06-27 16:49:21 +0200

    Initial import from https://github.com/angular/angular-seed

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+// Generated on 2014-02-19 using generator-angular 0.7.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+
+  // Load grunt tasks automatically
+ // require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+ // require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    yeoman: {
+      // configurable paths
+      app:  'app',
+      dist: 'dist'
+    },
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= yeoman.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: '0.0.0.0',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: false,
+          base: [
+            '.tmp',
+            '<%= yeoman.app %>'
+          ]
+        }
+      }
+    }
+ });
+
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('default', [
+    'server'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+This is a clone of https://github.com/angular/angular-seed project
+
+The .codenvy folder has been added as well Gruntfile.js with required JS libs in the package.json file so that Grunt runner could be used with that example
